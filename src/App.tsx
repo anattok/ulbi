efd7e86ff43949aafacb7a972658ea64bf04fa95
Anatoliy Tokarev <tokarev182@yandex.ru>
2024-02-11 18:21:32 +0300

    переключение темы с usecontext и usememo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import {  Routes, Route, Link } from "react-router-dom";
 import  {AboutPageAsync}  from "./pages/AboutPage/AboutPage.async";
 import  {MainPageAsync}  from "./pages/MainPage/MainPage.async";
-import {Suspense} from "react";
+import {Suspense, useContext} from "react";
 import "./styles/index.scss"
 import Counter from "./components/Counter/Counter";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
 
 export const App = () => {
+    const {theme, setTheme} = useContext(ThemeContext);
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+    }
+
     return (
-            <div className="app">
+            <div className={`app ${theme}`}>
+                <button onClick={toggleTheme}>toggleTheme</button>
                 <Counter/>
                 <Link to={"/"}>Главная</Link>
                 <Link to={"/about"}>About</Link>
